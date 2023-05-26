445b37852ad646188510b91cffca7c5eb25750a5
WumiDev <wumi2021@gmail.com>
2023-05-26 05:27:04 +0000

    initialize project

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
-const description = "<your-stackup-username>'s PolyAliens NFT collection.";
+const description = "WumiDev's PolyAliens NFT collection.";
 const baseUri = "ipfs://UPDATE";
 
 const layerConfigurations = [
